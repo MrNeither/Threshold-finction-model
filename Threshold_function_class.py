30b96c03c9f10489bff23de8066741ecc0f952f1
mr_neither <mr.neitherman@gmail.com>
2018-09-25 18:52:51 +0300

    made subclasses 2d and 3d thresholdfunction and drawer for 3d.

--- Threshold_function_class.py
+++ Threshold_function_class.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class ThresholdFunction:
@@ -12,8 +13,10 @@ class ThresholdFunction:
 
     MaxLinForm = 0
     MinLinForm = 0
-    __F = None
+    F = None
     Grid = None
+
+    methodType = None
 
     def __init__(self, dimension_im, rings_dim=None, coefficients=None, borders=None, cube=None):
         if cube is None:
@@ -38,10 +41,12 @@ class ThresholdFunction:
         self.Borders = np.sort(self.Borders)
 
         self.Grid = np.indices(self.DomainRings).reshape((len(self.DomainRings), -1)).T
-        self.__F = self.get_value(self.Grid).reshape(self.DomainRings)
+        self.F = self.get_value(self.Grid).reshape(self.DomainRings)
 
-    def write_options(self, namefile=None):
-        if namefile is None:
+        self.methodType = 'old'
+
+    def write_options(self, filename=None):
+        if filename is None:
             print("LinFormCoeff = ", self.LinFormCoeff)
             print("DomainRings = ", self.DomainRings)
             print("DomainDimension = ", self.DomainDimension)
@@ -49,9 +54,9 @@ class ThresholdFunction:
             print("MinLinForm = ", self.MinLinForm)
             print("Borders = ", self.Borders)
             print("ImageDimension = ", self.ImageDimension)
-            print("F = \n", self.__F)
+            print("F = \n", self.F)
         else:
-            with open(namefile, 'w') as f:
+            with open(filename, 'w') as f:
                 f.write("LinFormCoeff = " + str(self.LinFormCoeff) + "\n")
                 f.write("DomainRings = " + str(self.DomainRings) + "\n")
                 f.write("DomainDimension = " + str(self.DomainDimension) + "\n")
@@ -59,7 +64,7 @@ class ThresholdFunction:
                 f.write("MinLinForm = " + str(self.MinLinForm) + "\n")
                 f.write("Borders = " + str(self.Borders) + "\n")
                 f.write("ImageDimension = " + str(self.ImageDimension) + "\n")
-                f.write("F = \n"+ str(self.__F) + "\n")
+                f.write("F = \n" + str(self.F) + "\n")
             f.close()
 
     def normalization(self):
@@ -77,20 +82,12 @@ class ThresholdFunction:
             R[scalar >= border] = i + 1
         return R
 
-    def calculate_increase_coeff_type1(self, ind):
-        sum = 0
-        k = self.DomainRings[ind]
-        tempGrid = self.Grid.copy()
-        for e in range(0, k-1 + 1):
-            tempGrid[:, ind] = e
-            sum += (2*e + 1 - k) * self.__F[tuple(zip(*tempGrid))]
-        return np.sum(sum)/k
-
-    def geometric_algoritm(self):
+    def geometric_algorithm(self):
+        # Todo: Алгоритм должен работать с нормализованными данными
         pass
 
     def check(self):
-        l = list(map(lambda i: self.calculate_increase_coeff_type1(i), range(len(self.DomainRings))))
+        l = list(map(lambda i: self.calculate_increase_coeff(i), range(len(self.DomainRings))))
         F = list(map(lambda i: self.Grid[self.get_value(self.Grid) == i], range(self.ImageDimension)))
         F = [F[i] for i in range(len(F)) if len(F[i]) != 0]
         maxF = []
@@ -113,25 +110,58 @@ class ThresholdFunction:
         l = l - u + v
         return l
 
+    def setMethodType(self, type='old'):
+        self.methodType = type
+
+    def getMethodType(self):
+        return self.methodType
+
+    def calculate_increase_coeff(self,ind):
+        if self.methodType is 'old':
+            return self.old_method_increase_coeff(ind)
+        if self.methodType is 'new':
+            return self.new_method_increase_coeff(ind)
+
+        return None
+
+    def new_method_increase_coeff(self, ind):
+            sum = 0
+            k = self.DomainRings[ind]
+            tempGrid = self.Grid.copy()
+            for e in range(0, k):
+                tempGrid[:, ind] = e
+                sum += (2*e + 1 - k) * self.F[tuple(zip(*tempGrid))]
+            return np.sum(sum) / k
+
+    def old_method_increase_coeff(self, ind):
+            sum = 0
+            k = self.DomainRings[ind]
+            tempGrid = self.Grid.copy()
+            for e in range(0, k):
+                tempGrid[:, ind] = e
+                sum += (2 * e + 1 - k) * self.F[tuple(zip(*tempGrid))]
+            return np.sum(sum) / k
+
+
+class ThresholdFunction2D(ThresholdFunction):
+
     def draw2d(self, name=None):
         colors = "rbgcmyk"
         markers = "xo"
         F = list(map(lambda i: self.Grid[self.get_value(self.Grid) == i], range(self.ImageDimension)))
         F = [F[i] for i in range(len(F)) if len(F[i]) != 0]
-        for i in range(0,len(F)):
+        for i in range(0, len(F)):
             plt.plot(F[i][:, 0], F[i][:, 1], markers[i % 2], color=colors[i % len(colors)], markersize=4)
 
         for border in self.Borders:
-            X, Y = self.line1d(border, self.LinFormCoeff)
+            X, Y = self.border_line(border, self.LinFormCoeff)
             plt.plot(X, Y, linewidth=1, color='black')
 
-        l = list(map(lambda i: self.calculate_increase_coeff_type1(i), range(len(self.DomainRings))))
-        for i in range(0, len(F)-1):
-            b = (np.max(np.dot(F[i], l)) + np.min(np.dot(F[i + 1], l)))/2
-            X, Y = self.line1d(b, l)
+        l = list(map(lambda i: self.calculate_increase_coeff(i), range(len(self.DomainRings))))
+        for i in range(0, len(F) - 1):
+            b = (np.max(np.dot(F[i], l)) + np.min(np.dot(F[i + 1], l))) / 2
+            X, Y = self.border_line(b, l)
             plt.plot(X, Y, linewidth=1, color='red')
-
-
         plt.grid()
         if name is None:
             plt.show()
@@ -139,7 +169,7 @@ class ThresholdFunction:
             plt.savefig(name)
         plt.close()
 
-    def line1d(self, b, a):
+    def border_line(self, b, a):
         # a1x1+a2x2+b=0   =>  x1 = (b+a2x2)/-a1  x2 = (b+a1x1)/-a2
         X = []
         Y = []
@@ -150,12 +180,12 @@ class ThresholdFunction:
         if a2 == 0:
             a2 = 0.00000001
 
-        if 0 <= b/a1 <= k1 - 1:
-            X.append(b/a1)
+        if 0 <= b / a1 <= k1 - 1:
+            X.append(b / a1)
             Y.append(0)
-        if 0 <= b/a2 <= k2 - 1:
+        if 0 <= b / a2 <= k2 - 1:
             X.append(0)
-            Y.append(b/a2)
+            Y.append(b / a2)
         if 0 <= (b - a2 * (k2 - 1)) / a1 <= k1 - 1:
             X.append((b - a2 * (k2 - 1)) / a1)
             Y.append(k2 - 1)
@@ -163,3 +193,40 @@ class ThresholdFunction:
             X.append(k1 - 1)
             Y.append((b - a1 * (k1 - 1)) / a2)
         return X, Y
+
+
+class ThresholdFunction3D(ThresholdFunction):
+
+    def draw3d(self, name=None):
+        colors = "rbgcmyk"
+        markers = "xo"
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        for i in range(0, self.ImageDimension):
+            x, y, z = (self.F == i).nonzero()
+            ax.scatter(x, y, z, c=colors[i % len(colors)])
+
+        for border in self.Borders:
+            x, y, z = self.border_plane(border, self.LinFormCoeff)
+            ax.plot_surface(x, y, z, color='black', alpha=0.1)
+
+        ax.set_xlim3d([0, self.DomainRings[0] - 1])
+        ax.set_ylim3d([0, self.DomainRings[1] - 1])
+        ax.set_zlim3d([0, self.DomainRings[2] - 1])
+
+        plt.draw()
+        if name is None:
+            plt.show()
+        else:
+            plt.savefig(name)
+        plt.close()
+
+    def border_plane(self, d, a):
+        a1, a2, a3 = a
+        k1, k2, k3 = self.DomainRings
+        X = range(0, k1)
+        Y = range(0, k2)
+        X , Y = np.meshgrid(X, Y)
+        Z = (d - a1*X - a2*Y)/a3
+        return X, Y, Z
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from Threshold_function_class import ThresholdFunction as TFunc
+import Threshold_function_class as TFunc
 import numpy as np
 
-c = 0
-g = 0
-while c < 100:
-    t1 = TFunc(np.random.randint(2, 10), np.random.randint(3, 15, size=2))
-
-    # t1.show_options()
+bad = 0
+good = 0
+while good < 0 and bad < 0:
+    t1 = TFunc.ThresholdFunction(np.random.randint(2, 14), np.random.randint(3, 10, size=np.random.randint(2, 7)))
+    t1.normalization()
     if t1.check():
-        t1.write_options("infoGOOD " + str(g))
-        t1.draw2d("GOOD " + str(g))
-        g += 1
+        t1.setMethodType('new')
+        if t1.check():
+            pass
+        else:
+            print('BAD')
+        # t1.write_options("infoGOOD " + str(good))
+        # t1.draw2d("GOOD " + str(good))
+        good += 1
+        t1.normalization()
         # t1.draw2d()
 
     else:
-        t1.write_options("infoBAD " + str(c))
-        t1.draw2d("BAD " + str(c))
+        t1.setMethodType('new')
+        if t1.check():
+            print('GOOD')
+        # t1.write_options("infoBAD " + str(bad))
+        # t1.draw2d("BAD " + str(bad))
         # t1.draw2d()
-        c += 1
+        t1.normalization()
+        bad += 1
+
+t1 = TFunc.ThresholdFunction3D(np.random.randint(2, 14), np.random.randint(3, 10, size=3))
+# t1 = TFunc.ThresholdFunction3D(2, (2, 2, 2))
+t1.check()
+t1.draw3d('demo.png')
 print("complete")
+
